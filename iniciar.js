4a6351599cc69a40ca4a39613ea819d3269a790b
Leo Felippe <leofelippedias@hotmail.com.br>
2016-08-25 14:10:51 -0300

    testando verificacao de permissao

--- iniciar.js
+++ iniciar.js
@@ -49,6 +49,7 @@
     if(usuario != undefined){
       if (usuario.funcao && usuario.funcao.escopos && usuario.funcao.escopos[modelo]) {
         var bandeira = usuario.funcao.escopos[modelo].bandeira;
+        console.log('band  >> '+ bandeira + '  permissao :: '+ (bandeira & permissao));
         return bandeira & permissao;
       }
     }
